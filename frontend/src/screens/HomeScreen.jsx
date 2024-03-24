010987a7c117870735c34dc37de4078959def4a0
dann9109 <danielxstella@gmail.com>
2024-03-24 16:25:48 -0400

    message/loader

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,7 +2,8 @@
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productApiSlice';
-
+import  Loader from '../components/Loader';
+import  Message  from '../components/Message';
 
 const HomeScreen = () => {
     const { data: products, isLoading, error } = useGetProductsQuery();
@@ -10,8 +11,8 @@ const HomeScreen = () => {
     return (
         <div>
         {isLoading ? (
-            <h2>Loading...</h2>
-        ) : error ? (<div>{error.data.message || error.error}</div>) : (<><h1>Latest Products</h1>
+            <Loader />
+        ) : error ? (<Message variant='danger'>{error.data.message || error.error}</Message>) : (<><h1>Latest Products</h1>
             <Row>
                 {products.map(product => (() => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
