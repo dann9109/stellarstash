9add137ab695c6fd5859101d9498c6d021a3a8b4
dann9109 <danielxstella@gmail.com>
2024-03-27 10:23:21 -0400

    cart utils

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,17 +1,29 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
+import { Form,Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 import Rating from '../components/Rating';
 import axios from 'axios';
 import { useGetProductDetailsQuery } from '../slices/productApiSlice';
 import Message from '../components/Message';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
+import { addtoCart } from '../slices/cartSlice';
 
 
 const ProductScreen = () => {
     const { id: productId } = useParams();
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [qty, setQty] = useState(1);
+
+    const addtoCartHandler = () => {
+        dispatch(addtoCart({...product, qty}));
+        navigate('/cart');
+    };
     
     const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
@@ -61,8 +73,32 @@ const ProductScreen = () => {
                                             <Col><strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong></Col>
                                         </Row>
                                     </ListGroup.Item>
+                                    { product.countInStock > 0 && (
+                                        <ListGroup.Item>
+                                        <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control
+                            as='select'
+                            value={qty}
+                            onChange={(e) => setQty(Number(e.target.value))}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                                        </ListGroup.Item>
+                                    )}
                                     <ListGroup.Item>
-                                        <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                                        <Button className="btn-block" type="button" disabled={product.countInStock === 0}
+                                        onClick={addtoCartHandler}
+                                        >
                                             Add to Cart
                                         </Button>
                                     </ListGroup.Item>
---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,35 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {updateCart} from '../utils/cartUtils';
+
+const initialState = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cart')) : {cartItems: []};
+
+
+const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+}
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addtoCart: (state, action) => {
+            const item = action.payload;
+
+            const existItem = state.cartItems.find((x) => x.product === item.product);
+
+            if (existItem) {
+                state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x);
+            }else{
+                state.cartItems = [...state.cartItems, item];
+            }
+
+           return updateCart(state);
+          
+        },
+    },
+        
+    });
+   
+    
+export const {addtoCart} = cartSlice.actions;
+
+export default cartSlice.reducer;
